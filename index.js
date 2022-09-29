eb507aed429a4e56773171bd5c6923b2adf753ae
DuvanR11 <duriveramo@gmail.com>
2022-09-29 10:07:59 -0500

    ajustes

--- index.js
+++ index.js
@@ -1,54 +1,46 @@
 
-// Validación ingresando datos
-// const myArray = []
-// let bandera = 0;
+// Para ejucutar el algoritmo con parametro descomente la linea 4 a la 26
 
-// let tamaño = prompt("Ingrese numero de elementos")
-// let mitad = tamaño / 2
-// let temporal = tamaño - 1 
+// function parametro() {
+//   const myArray = []
+//   let tamaño = prompt("Ingrese longitud del Array")
+//   let mitad = tamaño / 2
+//   let temporal = tamaño - 1 
 
-// for (let i = 0; i < tamaño; i++) {
-//   let n = prompt(`ingrese numero ${i + 1}`)
-//   myArray.push(n)
+//   for (let i = 0; i < tamaño; i++) {
+//     let n = prompt(`ingrese numero ${i + 1}`)
+//     myArray.push(n)
+//   }
+
+
+//   for (let i = 0; i < mitad; i++){
+//     if(myArray[i] != myArray[temporal]){
+//       return console.log("Array No es Simetrico")
+//     }else{
+//       temporal--
+//     }
+//   }
+//   return console.log("Array Simetrico, Longitud", tamaño)
+
 // }
-
-// for (let i = 0; i < mitad && bandera == 0; i++){
-//   if(myArray[i] != myArray[temporal]){
-//     bandera = 1;
-//   }else{
-//     temporal--
-//   } 
-// }
-
-// if (bandera == 0) {
-//   console.log("longitud",tamaño)
-//   console.log("Arreglo es Simetrico")
-// } else {
-//   console.log("Arreglo es NO ES Simetrico")
-// }
+// parametro()
 
 
-// Validación datos quemados
-const myArray = [2, 8, 5, 11, 4, 25, 25, 4, 11, 5, 8, 2]
-let bandera = 0;
-let tamaño = myArray.length
-let mitad = tamaño / 2
-let temporal = tamaño - 1 
+// Los datos seran visualisados en la consola del navegador
+function simetrico() {
+  const myArray = [2, 8, 5, 11, 4, 25, 25, 4, 11, 5, 8, 2]
+  const tamaño = myArray.length
+  const mitad = tamaño / 2
+  let temporal = tamaño - 1 
 
 
-for (let i = 0; i < mitad && bandera == 0; i++){
-  if(myArray[i] != myArray[temporal]){
-    bandera = 1;
-  }else{
-    temporal--
-  } 
+  for (let i = 0; i < mitad; i++){
+    if(myArray[i] != myArray[temporal]){
+      return console.log("Array No es Simetrico")
+    }else{
+      temporal--
+    }
+  }
+  return console.log("Array Simetrico, Longitud", tamaño)
 }
-
-if (bandera == 0) {
-  console.log("longitud",tamaño)
-  console.log("Arreglo es Simetrico")
-} else {
-  console.log("Arreglo es NO ES Simetrico")
-}
-
-
+simetrico()
